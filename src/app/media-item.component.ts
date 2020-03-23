db0bdc02586b5a7262df2dcf53b4bd97b1aa2006
unknown <pim.krips@gmail.com>
2020-03-23 11:17:12 +0100

    Showing media-item with the @Input() decorator

--- src/app/media-item.component.ts
+++ src/app/media-item.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'mw-media-item',
@@ -7,6 +7,8 @@ import { Component } from '@angular/core';
 })
 
 export class MediaItemComponent {
+  @Input() mediaItem;
+
   onDelete() {
     console.log('deleted');
   }

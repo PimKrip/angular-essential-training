4c81c8be65741ab172a6d28db8dc9e9ed953be3d
unknown <pim.krips@gmail.com>
2020-03-24 21:31:48 +0100

    Put adding a new item in its own folder

--- src/app/new-item/media-item-form.component.ts
+++ src/app/new-item/media-item-form.component.ts
@@ -1,20 +1,23 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { MediaItemService } from './media-item.service';
-import { lookupListToken } from './providers';
+import { Router } from '@angular/router';
+import { MediaItemService } from '../media-item.service';
+import { lookupListToken } from '../providers';
 
 @Component({
   selector: 'mw-media-item-form',
   templateUrl: './media-item-form.component.html',
   styleUrls: ['./media-item-form.component.css']
 })
+
 export class MediaItemFormComponent implements OnInit {
   form: FormGroup;
 
   constructor(
     private formBuilder: FormBuilder,
     private mediaItemService: MediaItemService,
-    @Inject(lookupListToken) public lookupLists) {}
+    @Inject(lookupListToken) public lookupLists,
+    private router: Router) { }
 
   ngOnInit() {
     this.form = this.formBuilder.group({
@@ -49,6 +52,8 @@ export class MediaItemFormComponent implements OnInit {
 
   onSubmit(mediaItem) {
     this.mediaItemService.add(mediaItem)
-      .subscribe();
+      .subscribe(() => {
+        this.router.navigate(['/', mediaItem.medium])
+      });
   }
 }

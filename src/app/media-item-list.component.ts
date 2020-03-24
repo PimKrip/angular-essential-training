618cf110b54329ebd0e964ee3f9d8da3c596cb78
unknown <pim.krips@gmail.com>
2020-03-24 20:42:01 +0100

    Fixed all router links

--- src/app/media-item-list.component.ts
+++ src/app/media-item-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { MediaItemService, MediaItem } from './media-item.service';
 
 @Component({
@@ -6,14 +7,23 @@ import { MediaItemService, MediaItem } from './media-item.service';
   templateUrl: './media-item-list.component.html',
   styleUrls: ['./media-item-list.component.css']
 })
+
 export class MediaItemListComponent implements OnInit {
   medium = '';
   mediaItems: MediaItem[];
 
-  constructor(private mediaItemService: MediaItemService) {}
+  constructor(private mediaItemService: MediaItemService,
+    private activatedRoute: ActivatedRoute) { }
 
   ngOnInit() {
-    this.getMediaItems(this.medium);
+    this.activatedRoute.paramMap
+      .subscribe(paramMap => {
+        let medium = paramMap.get('medium');
+        if (medium.toLowerCase() === 'all') {
+          medium = ''
+        }
+        this.getMediaItems(medium);
+      });
   }
 
   onMediaItemDelete(mediaItem) {
